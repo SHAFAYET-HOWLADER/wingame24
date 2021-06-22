af89e25df634ede8981dd584b90e1020a242b6fb
SHAFAYET-HOWLADER <80398978+SHAFAYET-HOWLADER@users.noreply.github.com>
2021-06-22 16:32:12 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+//==========js start from here=========//
+
+//banner-slider
+$('.banner-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed:1000,
+    dots:false,
+    arrows:false,
+  });
+
+//img-slider
+$('.picture-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 500,
+  speed:1000,
+  dots:false,
+  arrows:false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
